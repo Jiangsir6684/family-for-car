c33067b983c5f62bf92b9ac440e066f4e5480bd7
jiangsir <610995699@qq.com>
2021-02-03 00:55:17 +0800

    add expain

--- FamilyCar/src/components/explain.vue
+++ FamilyCar/src/components/explain.vue
@@ -2,60 +2,49 @@
   <div class="explain">
     <el-container>
       <el-header>
-        <h1>积分说明</h1>
+        <h1>积分说明文档</h1>
       </el-header>
       <el-main class="mainStyle">
         <div class="partOne">
           <el-row>
             <el-col :span="24">
+                <el-tag class="h3Style" type="warning">积分构成:</el-tag>
+            </el-col>
+            <el-col :span="24">
               <ul>
-                <li class="float-l h3Style">积分构成:</li>
-                <li class="float-l">基础分</li>
-                <li class="float-l">倍数（权重系数、家庭代数）</li>
-                <li class="float-l">阶梯或轮候积分</li>
+                <li><el-tag class="h3Style">基础分</el-tag></li>
+                <li>主申请人:2分</li>
+                <li>副申请人:1分</li>
               </ul>
             </el-col>
             <el-col :span="24">
-              <ul>基础分
-                <li>主申请人</li>
-                <li>副申请人</li>
+              <ul>
+                <li><el-tag class="h3Style">倍数</el-tag></li>
+                <li>权重系数（即 成员与主申请人为夫妻关系的）：* 2</li>
+                <li>家庭代数（即 参与家庭摇号为几代同堂，最多为3代，最低1代）：*1 ～ *3</li>
               </ul>
             </el-col>
             <el-col :span="24">
-              <ul>倍数
-                <li>权重系数</li>
-                <li>家庭代数</li>
-              </ul>
-            </el-col>
-            <el-col :span="24">
-              <ul>阶梯或轮候积分
-                <li>油车</li>
-                <li>电车</li>
-                <li>油转电</li>
+              <ul>
+                <li><el-tag class="h3Style">阶梯或轮候积分</el-tag></li>
+                <li>油车：摇号倍数（可在北京市小客车指标调控管理信息系统）中查询</li>
+                <li>电车：根据申请编号查询编号到申请前一年的12.31日</li>
+                <li>油转电：油车倍数+电车轮候分</li>
               </ul>
             </el-col>
           </el-row>
-          <!-- <ul class="h3Style">积分构成
-            <li class="float-l h3Style">积分构成</li>
-            <li class="float-2">基础分</li>
-            <li class="float-2">倍数（权重系数、家庭代数）</li>
-            <li class="float-2">阶梯或轮候积分</li>
-          </ul> -->
         </div>
-        <div>
-          <h3>积分计算</h3>
+        <div class="partTwo">
+          <el-tag class="h3Style" type="warning">积分计算:</el-tag>
+          <el-col :span="24">
+            <ul>
+              <li>家庭总积分=[(主申请人总积分+配偶总积分)*权重系数(1～2)+上代积分总和+下代积分总和*家庭代数(1～3)</li>
+              <li>家庭总积分=</li>
+              <li>家庭总积分=</li>
+              <li>家庭总积分=</li>
+            </ul>
+          </el-col>
         </div>
-          <!-- <h3>积分构成</h3> -->
-          <!-- <span>111</span> -->
-          <!-- <el-row :gutter="20">
-            <el-col :span="3"><h3>积分构成</h3></el-col>
-            <el-col :span="3">基础分</el-col>
-            <el-col :span="7">倍数（权重系数、家庭代数）</el-col>
-            <el-col :span="5">阶梯或轮候积分</el-col>
-          </el-row> -->
-          <!-- <p>基础分</p>
-          <p>倍数</p>
-          <p>阶梯或轮候积分</p> -->
       </el-main>
     </el-container>
   </div>
@@ -100,11 +89,13 @@ export default {
     list-style: none;
     padding: 0px;
     margin: 0px;
+    text-align:left;
   }
   .h3Style{
     font-size: 20px;
     font-family: Times, serif;
     font-weight:bold;
+    margin-left: 0px;
   }
 }
 </style>
