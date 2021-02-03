c0dd174f12a378b97da0f6ddeb857202c4dc376c
jiangsir <610995699@qq.com>
2021-02-04 01:45:23 +0800

    add code

--- FamilyCar/src/components/calculate.vue
+++ FamilyCar/src/components/calculate.vue
@@ -1,28 +1,34 @@
 <template>
   <div class="explain">
-    <div class="message">积分计算</div>
-    <!-- <div>家庭总积分 = ｛｝</div> -->
-    <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
-    <el-dialog
-      title="积分计算问答"
-      :visible.sync="dialogVisible"
-      width="60%"
-      hight="30%"
-      :before-close="handleClose">
-      <!-- <span>主申请人是否参见过小客车（油车）摇号</span> -->
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
-      </span>
-    </el-dialog>
+    <!-- <div class="h3Style">积分计算</div>  -->
+    <el-container>
+      <el-header>
+        <h1>积分计算</h1>
+      </el-header>
+      <el-main class="mainStyle">
+        <el-col :span="24">
+          <el-tag class="h3Style">主申请人</el-tag>
+          <el-checkbox-group
+            v-model="checkedCities"
+            :min="1"
+            :max="2">
+            <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
+          </el-checkbox-group>
+        </el-col>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
+const cityOptions = ['上海', '北京', '广州', '深圳']
 export default {
   data () {
     return {
       dialogVisible: false,
+      // cityOptions: ['上海', '北京', '广州', '深圳'],
+      checkedCities: ['上海', '北京'],
+      cities: cityOptions,
       message: '家庭总积分= [(主申请人+配偶)*2+父+母+子+女]*3',
       nunber: '阶梯或基础轮候分 油转电 要 加油倍数  + 基础分'
     }
@@ -45,8 +51,15 @@ export default {
 
 <style scoped lang='less'>
 .explain{
-  font-size: 20px;
-  .message{
+  width: 80%;
+  margin:0 auto;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  border-radius:30px;
+    .h3Style{
+    font-size: 20px;
+    font-family: Times, serif;
+    font-weight:bold;
+    margin-left: 0px;
   }
 }
 </style>
